f362665825517e73f67c0b1b35267c42bbf4b4b4
MMA <MMA@IAZI.LOCAL>
2017-08-19 14:20:09 +0200

    Initial commit

---
+++ r/examples.R
@@ -0,0 +1,64 @@
+#==============================================================
+# Regression (realistic example based on diamonds data set)
+#==============================================================
+
+library(ggplot2)
+head(diamonds)
+
+# Prepare data set for modelling
+diamonds <- transform(as.data.frame(diamonds),
+                      log_price = log(price),
+                      log_carat = log(carat),
+                      cut = as.numeric(cut),
+                      color = as.numeric(color),
+                      clarity = as.numeric(clarity))
+
+r2 <- function(y, pred) {
+  1 - var(y - pred) / var(y)  
+}
+
+set.seed(3928272)
+.in <- sample(c(FALSE, TRUE), nrow(diamonds), replace = TRUE, p = c(0.15, 0.85))
+x <- c("log_carat", "cut", "color", "clarity", "depth", "table")
+x.train <- as.matrix(diamonds[.in, x])
+x.test <- as.matrix(diamonds[!.in, x])
+y.train <- diamonds[.in, "log_price"]
+y.test <- diamonds[!.in, "log_price"]
+dtrain <- xgb.DMatrix(x.train, label = y.train)
+dtest <- xgb.DMatrix(x.test, label = y.test)
+watchlist <- list(train = dtrain, test = dtest)
+
+param <- list(max_depth = 8, learning_rate = 0.01, nthread = 2, lambda = 0.2, 
+              objective = "reg:linear", eval_metric = "rmse", subsample = 0.7,
+              monotone_constraints = c(1, 1, 0, 1, 0, 0))
+
+fit_gbm <- xgb.train(param, dtrain, watchlist = watchlist, 
+                     nrounds = 850, early_stopping_rounds = 5)
+r2(y.train, predict(fit_gbm, dtrain)) # 0.9907
+r2(y.test, predict(fit_gbm, dtest)) # 0.9901
+
+par(mfrow = 3:2)
+partialPlot(fit_gbm, x.train, xname = "log_carat", subsample = 0.01)
+partialPlot(fit_gbm, x.train, xname = "cut", discrete.x = TRUE, subsample = 0.01)
+partialPlot(fit_gbm, x.train, xname = "color", discrete.x = TRUE, subsample = 0.01)
+partialPlot(fit_gbm, x.train, xname = "clarity", discrete.x = TRUE, subsample = 0.01)
+partialPlot(fit_gbm, x.train, xname = "depth", subsample = 0.01)
+partialPlot(fit_gbm, x.train, xname = "table", subsample = 0.01)
+
+
+#==============================================================
+# Multiclass prediction (toy example based on iris data set)
+#==============================================================
+
+dat <- as.matrix(iris[, 1:4])
+dtrain <- xgb.DMatrix(dat, label = as.numeric(iris$Species) - 1)
+
+param <- list(max_depth = 2, learning_rate = 0.1, objective = "multi:softprob", 
+              num_class = 3, eval_metric = "merror")
+
+fit_multi <- xgb.train(dtrain, params = param, nrounds = 100)
+
+par(mfrow = c(2, 2))
+for (nam in colnames(dat)) {
+  partialPlot(fit, dat, xname = nam, xlab = "", main = nam, which.class = 1)
+}
---
+++ r/partialPlot.R
@@ -0,0 +1,59 @@
+# Partial dependency plot for xgboost regression object
+#'
+#' @import xgboost
+#' @import graphics
+#' 
+#' @description Partial dependency plot for xgboost regression object. 
+#' Works as long objective is one of "reg:linear", "reg:logistic", "binary:logistic" or "multi:softprob"
+#' @author Michael Mayer, \email{mayer@consultag.ch}
+#' @param obj xgboost regression model
+#' @param pred.data Matrix to be used in prediction (no xgb.DMatrix)
+#' @param xname Name of column in \code{pred.data} according to that dependency plot is calculated
+#' @param n.pt Evaluation grid size (used only if x is not discrete)
+#' @param x.discrete If TRUE, the evaluation grid is set to the unique values of x
+#' @param subsample Fraction of lines in pred.data to be used in prediction
+#' @param which.class Which class if objective is "multi:softprob" (value from 0 to num_class - 1)
+#' @param xlab, ylab, main, type, ... Parameters passed to \code{plot}
+#' @param rug Should a rug plot be added?
+#' @param seed Random seed used if \code{subsample < 1}
+
+#' @return List of prepared objects
+partialPlot <- function(obj, pred.data, xname, n.pt = 19, discrete.x = FALSE, subsample = 1, which.class = NULL,
+                        xlab = deparse(substitute(xname)), ylab = "", type = if (discrete.x) "p" else "b",
+                        main = paste("Partial Dependence on", deparse(substitute(xname))), 
+                        rug = TRUE, seed = NULL, ...) {
+  stopifnot(inherits(obj, "xgb.Booster"), dim(pred.data) >= 1)
+  
+  if (subsample < 1) {
+    if (!is.null(seed)) {
+      set.seed(seed)
+    } 
+    n <- nrow(pred.data)
+    picked <- sample(n, trunc(subsample * n))
+    pred.data <- pred.data[picked, , drop = FALSE]
+    
+  }
+  xv <- pred.data[, xname, drop = FALSE]
+  
+  if (discrete.x) {
+    x <- unique(xv)
+  } else {
+    x <- seq(min(xv), max(xv), length = n.pt)
+  }
+  y <- numeric(length(x))
+  
+  for (i in seq_along(x)) {
+    pred.data[, xname] <- x[i]
+    
+    if (!is.null(which.class)) {
+      preds <- predict(obj, pred.data, reshape = TRUE)[, which.class, drop = FALSE]  
+    } else {
+      preds <- predict(obj, pred.data) 
+    }
+    
+    y[i] <- mean(preds)
+  }
+  
+  plot(x, y, xlab = xlab, ylab = ylab, main = main, type = type, ...)
+  rug(pmax(pmin(jitter(xv), max(xv)), min(xv)))
+}
